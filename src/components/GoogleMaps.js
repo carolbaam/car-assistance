9568f64d85388ed10060265518b778f40f25158f
carolbaam <carolinabaam@gmail.com>
2018-04-12 10:08:07 -0500

    subiendo mapa

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import { GoogleApiWrapper } from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
+const style = {
+   width: '100%',
+   height: '100%'
+}
+
+export class MapContainer extends React.Component {
+    render() {
+        return (
+            <Map google={this.props.google}
+            
+                style={style}
+                initialCenter={{
+                    lat: 40.854885,
+                    lng: -88.081807
+                }}
+            
+             zoom={14}>
+ 
+                {/* { <Marker onClick={this.onMarkerClick}
+                    name={'Current location'} />
+ 
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>} */}
+            </Map>
+        );
+    }
+    
+ }
+ 
+ export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAp0QWytHdPRYs_EJh1N-mWXHBL6HOhkvY'
+ })(MapContainer)
 // AIzaSyAp0QWytHdPRYs_EJh1N-mWXHBL6HOhkvY
 
-export default GoogleApiWrapper({
-    apiKey: AIzaSyAp0QWytHdPRYs_EJh1N-mWXHBL6HOhkvY
-})(MapContainer)
 
--- src/components/Principal.js
+++ src/components/Principal.js
@@ -3,11 +3,19 @@ import AppBar from './AppBar';
 import Form from './Form';
 import Button from './Button';
 import {Link} from 'react-router-dom';
+import GoogleApiWrapper from './GoogleMaps'
+;
+
+const style = {
+   width: '100%',
+   height: '100%'
+}
 
 const Main = () => (
   <div className="App">
     <AppBar name='Car Assistance'/>
    <h1>Aquí va tu mapa y tu buscador</h1>
+   <GoogleApiWrapper />
    <Link to={'/main'}><Button >Pide tu grúa</Button></Link>
     
   </div>
